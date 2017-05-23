ce10f9f5fa719c9021b386e2cae4c27fa4034b84
Ira Nefedova <iraonegina@yandex.ru>
2017-05-23 23:48:47 +0300

    headers fix

--- app/src/PokemonsService/PokemonsService.js
+++ app/src/PokemonsService/PokemonsService.js
@@ -2,14 +2,18 @@ angular
     .module('PokemonApp')
     .factory('PokemonsService', function($http) {
 // если применять common для всех запросов, pokeapi.co ругается
-            $http.defaults.headers.post = {
+            $http.defaults.headers.common = {
                 "application-id": "BBE2BD1F-9306-F389-FFCE-0D7310E74300",
                 "secret-key": "21A60E66-E921-977A-FF7D-AF3F75E3B400"
 
             };
 
-            $http.defaults.headers.delete = $http.defaults.headers.post;
-            $http.defaults.headers.put = $http.defaults.headers.post;
+            $http.defaults.headers.get = {
+                "application-id": "",
+                "secret-key": ""
+            };
+
+
             return {
 
                 getPokemons: function() {
